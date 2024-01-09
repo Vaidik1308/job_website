d6e35db67e77fc9126423d054b85e76ef0c29491
vaidik <vaidiksinghirwan@gamil.com>
2024-01-10 02:35:44 +0530

    designed the job cart section with responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,33 @@
+import JobListItem from "@/components/JobListItem"
+import prisma from "@/lib/prisma"
 
-export default function Home() {
+
+
+export default async function Home() {
+
+  const jobs = await prisma.job.findMany({
+    where:{ approved:true},
+    orderBy:{createdAt:"desc"}
+  }) 
+  // console.log(jobs);
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hello
+    <main className="max-w-5xl m-auto px-3 my-10 space-y-10">
+      <div className="space-y-5 text-center">
+        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">Developers Jobs</h1>
+        <p className="text-muted-foreground">Find Your Dream Job</p>
+      </div>
+      <section>
+        <div className="space-y-4">
+          {jobs && (
+            jobs.map((job) => (
+              <>
+                <JobListItem key={job.id} job={job}/>
+              </>
+            ))
+          )}
+        </div>
+      </section>
     </main>
   )
 }
